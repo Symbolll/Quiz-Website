2c8ece2bcef9aabccf10541f5bc4983383c4066e
Simge Kavalcı <61390121+Symbolll@users.noreply.github.com>
2021-05-31 14:31:12 +0300

    Add files via upload

---
+++ constants.js
@@ -0,0 +1,58 @@
+const RESULTS_MAP = {
+
+  //test1
+
+  'personality1': {
+    title: 'You are a person with storms inside ...',
+    contents: "You are a person who looks calmly from the outside, but storms break out. The more you try to stop these storms, the deeper you feel it sinking. At the same time, you cannot tell anyone about these storms and you live within yourself. It's like you've lost yourself in all these events. You are trying to find yourself and often think about uncertainty. What people around you say or think about you becomes very important to you. That's why there are a lot of times you pretend to be someone you are not. You fall into a big contradiction within yourself. Sometimes you question yourself in this meaninglessness. You are trying to remind yourself that you are the most valuable person. You say you don't have to change yourself for anyone. But sometimes life leads us to this just so that we don't feel lonely and that we feel loved by everyone. This is how we try to complete that great emptiness within us. This may be the biggest reason why you can't behave like yourself. That's why you find it difficult to be yourself around the people around you. This contradiction that you are living internally is actually tiring you. Sometimes you feel so upset that you had to act. You get angry with yourself, you get angry and you are afraid. But don't be angry, try to be kind to yourself. Because you are human too and you can fall into these contradictions. You can lose yourself, but it's up to you to find it.",
+  },
+  'personality2': {
+    title: 'Under your strong identity, you are a sensitive and fragile person ...',
+    contents: "Although you have a great emotional personality inside, you are a person who looks very strong from the outside. The one who sees you thinks as if you have no worries. However, you have a fragile and sensitive personality inside. You may not reflect your emotions to the people outside, but you fight a lot with your emotions inside. With that strong, indestructible and solid personality, there is someone whose heart is shattered and his emotions are tampered with. Sometimes your emotions take you so captive that you feel sorry for yourself. As you feel your loneliness even more deeply and cannot share it with anyone, you break down. Although you try to hide your feelings, it comes to light in those lonely moments. You are a person full of goodness and purity and you do not want to offend anyone. In fact, you are ready to do your best for the happiness of your loved ones. Often times you think everyone is like you and you get disappointed. You no longer trust people. Now you don't have as pure and clean feelings as before. When you see people's dark sides and touch you, you have learned to respond to them. Although you have pure feelings of goodness in your heart, you try not to reflect it to the outside in this way. You take a strong, indestructible and reckless attitude. Because you think people will disappoint you at any moment. This is how you protect yourself. Because you don't want to be offended again and let people play with your emotions. You took enough damage, after that you try to protect yourself as much as possible.",
+  },
+  'personality3': {
+    title: 'You are a lonely personality ...',
+    contents: "Although there are people who think you are close to you, you feel incredibly lonely and vulnerable. You think that nobody really understands you and is taking all your pain and suffering on their own. At night, you realize how lonely you are trying to deal with all these problems. You are aware of how far the people you call close are actually from you. There is no one you can literally open inside. That's why you are trying to deal with all your emotions and feelings alone. You realize that in this life, no one but yourself will be of any good use to you. You no longer have any expectations. Neither against life nor against people. You overcome your difficulties and happiness by yourself. You never, ever believe in the sincerity and friendliness of people. You have lost faith in everyone inside. That's why you are so far away from everyone, you are alone and alone ...",
+  },
+
+
+  //test2
+
+  'city1': {
+    title: 'Barcelona!',
+    contents: "Your city is none other than the autonomous capital of Catalonia, the pearl of the Mediterranean, Barcelona!",
+  },
+  'city2': {
+    title: 'Rome!',
+    contents: "Being the cradle of many civilizations and being one of the first cities to come to mind when history is mentioned, Rome is 100% compared to the city of your dreams!",
+  },
+  'city3': {
+    title: 'Amsterdam!',
+    contents: "Here you have everything you are looking for: freedom, architecture, art, natural beauty ... Amsterdam is the greatest candidate to become the city you feel like belonging to."
+  },
+
+  //test3
+
+  'hoby1': {
+    title: ' It is time to reveal your inner artist!',
+    contents: "You should definitely meet some art in this closure! That's why we say your new hobby is both very colorful and very enjoyable: Watercolor! Yes, it may be scary on your lap at first, but we are sure that you will improve as you do, because we know you have this perseverance. Well, you are also conscious, you spend time with yourself and listen to your inner voice. You can create perfect works by dissolving what you think and the boredom of being closed at home with art!",
+  },
+  'hoby2': {
+    title: 'Maybe it is time to make your own world!',
+    contents: "We were stuck at home, we forgot outside, we even forgot to walk in the park ... But during this period, we also found something to bring home: Model making! Yes, it requires a lot of patience, but you can do it yourself. Your logic and love of calculations will make everything easier, so you can see you have erected a huge building or a school with a big garden in front of it. You can even make your own workplace. Both creativity and logic come together. It is really a hobby that will keep you busy until May 17!",
+  },
+
+  //test4
+  'plant1': {
+    title: 'Blue Thistle of the Alps!',
+    contents: "You have a pretty tough personality and you don't come out easily to others. You look innocent, but you get angry quickly and aren't afraid to speak up.",
+  },
+  'plant2': {
+    title: 'Sunflower!',
+    contents: "The first thing a person who looks at your face sees in you is a glimmer of hope! You also have a very strong character. We can also say that he is a very good confidant. A person who does not love you has to turn and look at himself. Because the problem is most likely in itself.",
+  },
+  'plant3': {
+    title: 'Indian Mustard Flower!',
+    contents: "The best thing we can say for you is that you are an unbearable romantic. You want to live every moment of this life with love. Perhaps you are one of those who enjoy youth the best. However, over the years, you will settle down and turn into a calm person.",
+  }
+};
